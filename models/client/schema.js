5f8666ea37d73e434d4be3aca50bf0e99cba3b33
Adrian <Adrian@DESKTOP-JE8H084>
2020-04-03 18:46:56 +0300

    deleted useless comments

--- models/client/schema.js
+++ models/client/schema.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const menuSchema = require('../../menu/schema.js'); // to do
-//i need clientSchema.preferences too look just like the providerSchema.courses.course ?
 
 const clientSchema = mongoose.Schema({
 	preferences: [
@@ -13,10 +12,7 @@ const clientSchema = mongoose.Schema({
 		type: [String],
 		required: false,
 	},
-	//i don't know for sure
-	//if we'll use google maps for location? i guess it should look diffrent
-	//do we need separate proprieties for location
-	//or do we just store it into a String?
+	//we'll use google maps api
 	location: {
 		type: String,
 		required: false,
@@ -49,8 +45,6 @@ const clientSchema = mongoose.Schema({
 				course: {
 					type: menuSchema.courses,
 					required: true,
-					//nu sunt sigur, insa daca apare un idProvider, probabil
-					//trebuie sa apara is cel putin o comanda, deci va fi required
 				},
 
 				price: {
