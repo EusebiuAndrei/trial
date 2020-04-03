cc3bcb71863a6093f9c5aaf2a055a192e10fcc1d
Adrian <Adrian@DESKTOP-JE8H084>
2020-04-03 18:33:42 +0300

    constraint added to schedulemodel files cleanup

--- models/schedule/schema.js
+++ models/schedule/schema.js
@@ -6,17 +6,19 @@ const scheduleSchema = mongoose.Schema({
 			day: {
 				type: String,
 				required: true,
+				lowercase: true,
+				trim: true,
 				min: 3,
 			},
 			startHour: {
 				type: String, // 10 am/pm type
 				required: true,
-				min: 5,
+				min: 4,
 			},
 			endHour: {
 				type: String,
 				required: true,
-				min: 5,
+				min: 4,
 			},
 		},
 	],
