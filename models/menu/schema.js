b031586be0ad82e2f4837bb3381ba77689bbcc15
tsipoteanu99 <tsipoteanu99@gmail.com>
2020-04-04 21:56:26 +0300

    adaugare course vector

--- models/menu/schema.js
+++ models/menu/schema.js
@@ -1,28 +1,33 @@
 const mongoose = require('mongoose');
 
 const menuSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	category: {
-		type: [String],
-		required: true,
-	},
-	price: {
-		type: Number,
-		required: true,
-	},
-	ingredients: {
-		type: [String],
-	},
-	allergenes: {
-		type: [String],
-	},
+	courses: [
+		{
+			name: {
+				type: String,
+				required: true,
+			},
+			category: {
+				type: [String],
+				required: true,
+			},
+			price: {
+				type: Number,
+				required: true,
+			},
+			ingredients: {
+				type: [String],
+			},
+			allergenes: {
+				type: [String],
+			},
+			
+		},
+		
+	],
 	idProvider: {
 		type: mongoose.Types.ObjectId,
 		require: true,
-	},
 });
 
 module.exports = menuSchema;
