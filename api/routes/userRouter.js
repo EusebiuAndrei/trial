585ed1078fd3ea57513f11da7d6f689a318a6173
Eusebiu Gagea <eusebiuandrei_gagea@yahoo.co.uk>
2020-04-12 19:35:12 +0300

    enw change

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,16 +1,28 @@
 const { Router } = require('express');
 const { celebrate } = require('celebrate');
 const { userService } = require('../../services/index');
-const { auth } = require('../middlewares/index');
+const { auth, upload } = require('../middlewares/index');
 const { imageService } = require('../../services/index');
 const sharp = require('sharp');
-const upload = imageService.uploadImg();
+//const upload = imageService.uploadImg();
 const uuid = require('uuid');
 const Logger = require('../../loaders/logger');
 // validation schemas
 const { userValidationSchema } = require('../../models/index');
 
 const router = Router();
+
+const path = require('path');
+
+router.post('/upload', upload.single('image'), async (req, res) => {
+	const imagePath = path.join(__dirname, '/public/images');
+	const fileUpload = new Resize(imagePath);
+	if (!req.file) {
+		res.status(401).json({ error: 'Please provide an image' });
+	}
+	const filename = await fileUpload.save(req.file.buffer);
+	return res.status(200).json({ name: filename });
+});
 
 //image routs here we've made the tests.
 router.post(
