f8a560be730eaa2d9aef46f89d23dd78d8a96b1a
Adrian <Adrian@DESKTOP-JE8H084>
2020-04-25 20:38:33 +0300

    register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,50 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-	return <div className="App"></div>;
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Form>
+					<Form.Group>
+						<Form.Text>
+							<h1>Register Form</h1>
+						</Form.Text>
+					</Form.Group>
+					<Form.Group controlId="formBasicEmail">
+						<Form.Label>Email address</Form.Label>
+						<Form.Control
+							type="email"
+							placeholder="Enter email"
+						/>
+						<Form.Text className="text-muted">
+							We'll never share your email with anyone
+							else.
+						</Form.Text>
+					</Form.Group>
+
+					<Form.Group controlId="formBasicPassword">
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							type="password"
+							placeholder="Password"
+						/>
+					</Form.Group>
+					<Form.Group controlId="formBasicCheckbox">
+						<Form.Check
+							type="checkbox"
+							label="Check me out"
+						/>
+					</Form.Group>
+					<Button variant="primary" type="submit">
+						Register
+					</Button>
+				</Form>
+			</div>
+		);
+	}
 }
 
 export default App;
