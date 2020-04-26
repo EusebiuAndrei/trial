cd4d09aac5721fefc9804dd468ba8ef0bf4585eb
Stefan Rusu <>
2020-04-27 00:11:04 +0300

    added Users frontend/same as provider

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,45 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import User from './User';
-import Client from './Client';
-import Provider from './Provider';
+import User from "./User";
+import Client from "./Client";
+import Provider from "./Provider";
 
 class Profile extends Component {
-	state = {
-		email: '',
-		name: '',
-		role: 'Provider',
-		token: '',
-		details: {
-			location: {
-				latitude: '123',
-				longitude: '-189',
-				adress: 'Mihail Kogalniceanu',
-			},
-			CUI: '1242341353463563',
-			type: 'Restaurant',
-			description:
-				'Un restaurant foarte frumos si foarte interesant as putea zice',
-			images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
-			rating: 5,
-			priceCategory: 'Medium',
-			specials: ['Pizza', 'Pasta', 'Fish', 'Parfais'],
-			tables: '6',
-		},
-	};
+  state = {
+    email: "test@yahoo.com",
+    name: "test",
+    role: "Provider",
+    token: "",
+    details: {
+      location: {
+        latitude: "123",
+        longitude: "-189",
+        adress: "Mihail Kogalniceanu",
+      },
+      CUI: "1242341353463563",
+      type: "Restaurant",
+      description:
+        "Un restaurant foarte frumos si foarte interesant as putea zice",
+      images: ["image1.jpg", "image2.jpg", "image3.jpg"],
+      rating: 5,
+      priceCategory: "Medium",
+      specials: ["Pizza", "Pasta", "Fish", "Parfais"],
+      tables: "6",
+    },
+  };
 
-	render() {
-		return (
-			<div>
-				<User />
-				{this.state.role === 'Client' ? (
-					<Client data={this.state.details}></Client>
-				) : (
-					<Provider data={this.state.details}></Provider>
-				)}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <User data={this.state}></User>
+        {this.state.role === "Client" ? (
+          <Client data={this.state.details}></Client>
+        ) : (
+          <Provider data={this.state.details}></Provider>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Profile;
--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,8 +1,80 @@
-import React from 'react';
-import {} from 'react-bootstrap';
+import React, { useState } from "react";
+import {
+  Card,
+  Button,
+  Container,
+  Row,
+  FormControl,
+  Form,
+  Col,
+} from "react-bootstrap";
 
-const User = () => {
-	return <div></div>;
+const User = ({ data }) => {
+  const handleInput = (e, type) => {
+    if (type === "longitude" || type === "latitude" || type === "adress")
+      data.location[type] = e.target.value;
+    else if (type === "specials") {
+      data[type].push(e.target.value);
+    } else data[type] = e.target.value;
+  };
+
+  const [toInputDescription, setToInputDescription] = useState(false);
+  const handleEditDescriptionButton = () => {
+    setToInputDescription(!toInputDescription);
+  };
+  return (
+    <div>
+      <Form>
+        <Container>
+          <Card>
+            <Card.Header className="providerCardHeader">
+              <h5 className="cardTitle">Detaliile userului</h5>
+              <Button
+                className="cardEdit"
+                variant="primary"
+                onClick={handleEditDescriptionButton}
+              >
+                {!toInputDescription && "Edit"}
+                {toInputDescription && "Done"}
+              </Button>
+            </Card.Header>
+
+            <Card.Body>
+              <Row className="cardRow" xs={17}>
+                <Form.Group className="formElementProvider" controlId="type">
+                  <Form.Label as={Col} xs={6}>
+                    <b>Email</b>
+                  </Form.Label>
+                  {!toInputDescription && <p>{data.email}</p>}
+                  {toInputDescription && (
+                    <FormControl
+                      as="textarea"
+                      onChange={(e) => handleInput(e, "email")}
+                    />
+                  )}
+                </Form.Group>
+              </Row>
+
+              <Row className="cardRow" xs={17}>
+                <Form.Group className="formElementProvider" controlId="type">
+                  <Form.Label as={Col} xs={6}>
+                    <b>Username</b>
+                  </Form.Label>
+                  {!toInputDescription && <p>{data.name}</p>}
+                  {toInputDescription && (
+                    <FormControl
+                      as="textarea"
+                      onChange={(e) => handleInput(e, "name")}
+                    />
+                  )}
+                </Form.Group>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Container>
+      </Form>
+    </div>
+  );
 };
 
 export default User;
