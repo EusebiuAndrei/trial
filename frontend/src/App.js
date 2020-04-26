f3f37335a6a85ea53199cc1d00667637fd09102d
Eusebiu Gagea <eusebiuandrei_gagea@yahoo.co.uk>
2020-04-26 20:24:33 +0300

    fds

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Profile from './components/Profile';
+import Register from './components/Register';
 
 function App() {
 	return (
 		<div className="App">
 			<Profile></Profile>
+			<Register></Register>
 		</div>
 	);
 }
