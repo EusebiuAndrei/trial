9d98acc7368ecaee40cbce471d3cdea91c9bed64
Stefan Rusu <>
2020-04-27 20:15:10 +0300

    3 modele de restaurante

---
+++ backend/fixtures/restaurant.js
@@ -0,0 +1,78 @@
+state = {
+	email: 'panoramic@hotelunirea.ro',
+	name: 'Panoramic Restaurant',
+	role: 'Provider',
+	token: 'asdpoi123gASC890PLM3',
+
+	location: {
+		latitude: '47.166685',
+		longitude: '27.580516',
+		adress: 'Piața Unirii nr. 5, Iași 700056',
+	},
+	CUI: '10164493',
+	type: 'Restaurant',
+	description:
+		'The Panoramic Restaurant is located at 55 metres height, with a breathtaking 360° panorama of Iasi. ',
+	images: [
+		'https://www.google.com/search?q=restaurant+panoramic+iasi&rlz=1C5CHFA_enUS867US867&sxsrf=ALeKk00Dq8KQzJQPWryAR9nPvKWIw0CYEg:1588005470334&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjloNevhYnpAhUVHHcKHQ-vA1cQ_AUoAXoECBQQAw&biw=1440&bih=821#imgrc=tKjxcM3oeiTzFM',
+		'https://www.google.com/url?sa=i&url=https%3A%2F%2Fall.biz%2Fro-ro%2Frestaurant-panoramic-s7365&psig=AOvVaw3gPQ5yW0Y2emMBZXw33VcS&ust=1588091918490000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDjoMeFiekCFQAAAAAdAAAAABAJ',
+		'https://www.google.com/url?sa=i&url=https%3A%2F%2Finland.ro%2Fportfolio%2Frestaurant-panoramic-hotel-unirea-iasi-reamenajare-design-interior%2F&psig=AOvVaw3gPQ5yW0Y2emMBZXw33VcS&ust=1588091918490000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDjoMeFiekCFQAAAAAdAAAAABAV',
+	],
+	rating: 4.6,
+	priceCategory: 'High',
+	specials: ['Pasta & Risotto', 'Traditinal', 'Soups', 'Salads'],
+	tables: '37',
+};
+
+state2 = {
+	email: 'office@vivorestaurants.com',
+	name: 'Vivo',
+	role: 'Provider',
+	token: 'as8Cplhn41CMLCeraf25',
+
+	location: {
+		latitude: '47.156272',
+		longitude: '27.593321',
+		adress: 'Strada Moldovei 20, Iași 700046',
+	},
+	CUI: '32744904',
+	type: 'Restaurant',
+	description:
+		'Din 2015 nominalizat in fiecare an la categoria CEL MAI BUN RESTAURANT STEAKHOUSE/ BURGER in cadrul Galelor Horeca Awards ',
+	images: [
+		'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vivofoodbar.com%2F&psig=AOvVaw1Owc0ej3xE9eJEwZ2Hmh_r&ust=1588092884657000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIC3wZSJiekCFQAAAAAdAAAAABAS',
+		'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nwradu.ro%2F2015%2F04%2Fturul-burgerilor-game-of-dijons-la-vivo-fusion-food-bar%2F&psig=AOvVaw1Owc0ej3xE9eJEwZ2Hmh_r&ust=1588092884657000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIC3wZSJiekCFQAAAAAdAAAAABAd',
+		'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.zilesinopti.ro%2Flocuri%2F8069%2Fvivo&psig=AOvVaw1Owc0ej3xE9eJEwZ2Hmh_r&ust=1588092884657000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIC3wZSJiekCFQAAAAAdAAAAABAj',
+	],
+	rating: 4.7,
+	priceCategory: 'Medium',
+	specials: ['Burgers', 'Stake House'],
+	tables: '25',
+};
+
+state3 = {
+	email: 'sandacuciureanu@yahoo.com',
+	name: 'Cantina „Titu Maiorescu”',
+	role: 'Provider',
+	token: 'noiWFc26mPLMe3vC41f2',
+
+	location: {
+		latitude: '47.174452',
+		longitude: '27.569752',
+		adress:
+			'Campus Titu Maiorescu, Strada Titu Maiorescu 7-9, Iași 700461',
+	},
+	CUI: '38104224',
+	type: 'Canteen',
+	description:
+		'Studenţii Universităţii „Alexandru Ioan Cuza” pot lua masa la cantina din campusul studenţesc „Titu Maiorescu”.',
+	images: [
+		'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bzi.ro%2Fcantina-titu-maiorescu-a-fost-inchisa-231847&psig=AOvVaw0U4qRH0YffXsexIm5Bn_K6&ust=1588092970456000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLC2vr2JiekCFQAAAAAdAAAAABAD',
+		'https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.ziare.com%2Flocuri-de-munca%2Fangajatori%2Fstudentii-din-iasi-se-pot-angaja-la-cantina-918443&psig=AOvVaw0U4qRH0YffXsexIm5Bn_K6&ust=1588092970456000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLC2vr2JiekCFQAAAAAdAAAAABAO',
+		'https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.cuzanet.ro%2Facademic%2Fcampus%2Facces-cantina-uaic.html&psig=AOvVaw0U4qRH0YffXsexIm5Bn_K6&ust=1588092970456000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLC2vr2JiekCFQAAAAAdAAAAABAU',
+	],
+	rating: 3.8,
+	priceCategory: 'Low',
+	specials: ['Home cooked', 'Fast Food', 'Traditional'],
+	tables: '100',
+};
