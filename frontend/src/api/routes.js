80321dbdd3af615e03baeea211a5f343519e4fc7
Eusebiu Gagea <eusebiuandrei_gagea@yahoo.co.uk>
2020-04-28 12:36:07 +0300

    added routes

---
+++ frontend/src/api/routes.js
@@ -0,0 +1,11 @@
+const routes = [
+	'http://localhost:4000/api/users/uploadSingle', // Sipoteanu
+	'http://localhost:4000/api/users/uploadMultiple', // Tipa
+	'http://localhost:4000/api/users/register', // Rusu
+	'http://localhost:4000/api/users/logout', // Cristi
+	'http://localhost:4000/api/users/profile', // Geo
+	'http://localhost:4000/api/menus', // Geo
+	'http://localhost:4000/api/users/lostpassword',
+	'http://localhost:4000/api/users/changepassword',
+	'http://localhost:4000/api/users/confirm:token',
+];
