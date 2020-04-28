100362f314d97e9db6f1beaa47f311f82723cf70
Georgiana <stativa50@gmail.com>
2020-04-28 14:18:54 +0300

    Menus and profile route

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,67 +1,108 @@
-import axios from 'axios';
-import jwt from 'jsonwebtoken';
-import { setAuthorizationToken } from '../helpers/auth';
+import axios from "axios";
+import jwt from "jsonwebtoken";
+import { setAuthorizationToken } from "../helpers/auth";
 
 const login = async (email, password) => {
-	try {
-		const {
-			data: { user, token },
-		} = await axios({
-			method: 'post',
-			url: 'http://localhost:4000/api/users/login',
-			data: {
-				email,
-				password,
-			},
-		});
+  try {
+    const {
+      data: { user, token },
+    } = await axios({
+      method: "post",
+      url: "http://localhost:4000/api/users/login",
+      data: {
+        email,
+        password,
+      },
+    });
 
-		localStorage.setItem('userToken', token);
+    localStorage.setItem("userToken", token);
 
-		return { success: true, user };
-	} catch (error) {
-		return { success: false, errorMessage: error.message };
-	}
+    return { success: true, user };
+  } catch (error) {
+    return { success: false, errorMessage: error.message };
+  }
 };
 
 const getUser = async () => {
-	try {
-		const token = localStorage.getItem('userToken');
-		const { _id } = jwt.decode(token);
-		setAuthorizationToken(token);
+  try {
+    const token = localStorage.getItem("userToken");
+    const { _id } = jwt.decode(token);
+    setAuthorizationToken(token);
 
-		const {
-			data: {
-				data: { user },
-			},
-		} = await axios({
-			method: 'get',
-			url: `http://localhost:4000/api/users/${_id}`,
-		});
+    const {
+      data: {
+        data: { user },
+      },
+    } = await axios({
+      method: "get",
+      url: `http://localhost:4000/api/users/${_id}`,
+    });
 
-		return { success: true, user };
-	} catch (error) {
-		return { success: false, errorMessage: error.message };
-	}
+    return { success: true, user };
+  } catch (error) {
+    return { success: false, errorMessage: error.message };
+  }
 };
 
 const getAllUsers = async () => {
-	try {
-		const token = localStorage.getItem('userToken');
-		setAuthorizationToken(token);
+  try {
+    const token = localStorage.getItem("userToken");
+    setAuthorizationToken(token);
 
-		const {
-			data: {
-				data: { users },
-			},
-		} = await axios({
-			method: 'get',
-			url: `http://localhost:4000/api/users/`,
-		});
+    const {
+      data: {
+        data: { users },
+      },
+    } = await axios({
+      method: "get",
+      url: `http://localhost:4000/api/users/`,
+    });
 
-		return { success: true, users };
-	} catch (error) {
-		return { success: false, errorMessage: error.message };
-	}
+    return { success: true, users };
+  } catch (error) {
+    return { success: false, errorMessage: error.message };
+  }
 };
 
-export { login, getUser, getAllUsers };
+const menus = async () => {
+  try {
+    const token = localStorage.getItem("userToken");
+    setAuthorizationToken(token);
+    const {
+      data: {
+        data: { menus },
+      },
+    } = await axios({
+      method: "get",
+      url: `http://localhost:4000/api/menus/`,
+    });
+
+    return { success: true, menus };
+  } catch (error) {
+    return { success: false, errorMessage: error.message };
+  }
+};
+
+const profile = async (userData) => {
+  try {
+    const token = localStorage.getItem("userToken");
+    setAuthorizationToken(token);
+    const {
+      data: {
+        data: { userDetails },
+      },
+    } = await axios({
+      method: "post",
+      url: `http://localhost:4000/api/users/profile`,
+      data: {
+        userData,
+      },
+    });
+    console.log(userDetails);
+    return { success: true, userDetails };
+  } catch (error) {
+    return { success: false, errorMessage: error.message };
+  }
+};
+
+export { profile, menus, login, getUser, getAllUsers };
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,59 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import User from './User';
-import Client from './Client';
-import Provider from './Provider';
+import User from "./User";
+import Client from "./Client";
+import Provider from "./Provider";
 
 class Profile extends Component {
-	// state = {
-	// 	email: 'user1@gmail.com',
-	// 	name: 'User1',
-	// 	role: 'Provider',
-	// 	token: '',
-	// 	details: {
-	// 		location: {
-	// 			latitude: '123',
-	// 			longitude: '-189',
-	// 			adress: 'Mihail Kogalniceanu',
-	// 		},
-	// 		CUI: '1242341353463563',
-	// 		type: 'Restaurant',
-	// 		description:
-	// 			'Un restaurant foarte frumos si foarte interesant as putea zice',
-	// 		images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
-	// 		rating: 5,
-	// 		priceCategory: 'Medium',
-	// 		specials: ['Pizza', 'Pasta', 'Fish', 'Parfais'],
-	// 		tables: '6',
-	// 	},
-	// };
-	state = {
-		email: 'user1@gmail.com',
-		name: 'User1',
-		role: 'Client',
-		token: '',
-		details: {
-			location: {
-				latitude: '4000',
-				longitude: '234',
-			},
-			preferences: ['lapte', 'oua'],
-			allergies: ['gluten', 'polen', 'alte chestii'],
-		},
-	};
+  // state = {
+  // 	email: 'user1@gmail.com',
+  // 	name: 'User1',
+  // 	role: 'Provider',
+  // 	token: '',
+  // 	details: {
+  // 		location: {
+  // 			latitude: '123',
+  // 			longitude: '-189',
+  // 			adress: 'Mihail Kogalniceanu',
+  // 		},
+  // 		CUI: '1242341353463563',
+  // 		type: 'Restaurant',
+  // 		description:
+  // 			'Un restaurant foarte frumos si foarte interesant as putea zice',
+  // 		images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+  // 		rating: 5,
+  // 		priceCategory: 'Medium',
+  // 		specials: ['Pizza', 'Pasta', 'Fish', 'Parfais'],
+  // 		tables: '6',
+  // 	},
+  // };
+  state = {
+    email: "user1@gmail.com",
+    name: "User1",
+    role: "Client",
+    token: "",
+    details: {
+      location: {
+        latitude: "4000",
+        longitude: "234",
+      },
+      preferences: ["lapte", "oua"],
+      allergies: ["gluten", "polen", "alte chestii"],
+    },
+  };
 
-	render() {
-		return (
-			<div>
-				<User data={this.state}></User>
-				{this.state.role === 'Client' ? (
-					<Client data={this.state.details}></Client>
-				) : (
-					<Provider data={this.state.details}></Provider>
-				)}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <User data={this.state}></User>
+        {this.state.role === "Client" ? (
+          <Client data={this.state.details}></Client>
+        ) : (
+          <Provider data={this.state.details}></Provider>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Profile;
