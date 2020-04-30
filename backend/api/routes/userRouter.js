d210671afbe5fd2c2cbd9fc1324ef89d1bba492d
Cozma <cozmacatalin33@gmail.com>
2020-04-30 23:58:04 +0300

    done add photos

--- backend/api/routes/userRouter.js
+++ backend/api/routes/userRouter.js
@@ -11,6 +11,32 @@ const { userValidationSchema } = require('../../models/index');
 
 const router = Router();
 
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+   destination: "./uploads/",
+   filename: function(req, file, cb){
+      cb(null,"IMAGE-" + Date.now() + path.extname(file.originalname));
+   }
+});
+
+const myUpload = multer({
+   storage: storage,
+   limits:{fileSize: 1000000},
+}).single("myImage");
+
+router.post('/uploadCozma', function (req, res) {
+    myUpload(req, res, function (err) {
+        console.log("Request ---", req.body);
+        console.log("Request file ---", req.file);
+
+		if(!err) {
+            return res.send(200).end();
+        }
+    })
+})
+
 router.post(
 	'/uploadSingle',
 	upload.single('image'),
@@ -101,6 +127,7 @@ router.post(
 		body: userValidationSchema,
 	}),
 	async function (req, res) {
+		console.log("salut");
 		const result = await userService.login(req.body);
 		const statusCode = result.success ? 200 : 400;
 		res.status(statusCode).json(result);
