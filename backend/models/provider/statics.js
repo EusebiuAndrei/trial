4db520cd13858c9e4a969073643edbc897d47464
Adrian <Adrian@DESKTOP-JE8H084>
2020-05-01 15:58:06 +0300

    statics method used

--- backend/models/provider/statics.js
+++ backend/models/provider/statics.js
@@ -1,5 +1,13 @@
 // Create functions that will represent schema statics
+const findByTags = async function (tags) {
+	let filter = {};
+	if (tags.length) {
+		filter = { specials: { $all: tags } };
+	}
+	return this.find(filter);
+};
 
 module.exports = {
 	// Put them all here
+	findByTags,
 };
